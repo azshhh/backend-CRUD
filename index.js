8de0d9c52d5caad6151be22eee5e81ff46906299
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-06 21:46:03 +0530

    Creating a Schema

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
+// Connecting our application to MongoDb server
 mongoose.connect('mongodb://localhost/crud-operation')
     .then(() => console.log('Connecting to MongoDB...'))
     .catch(error => console.error('MongoDb connection failed.', error));
 
+// Creating a Schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+});
